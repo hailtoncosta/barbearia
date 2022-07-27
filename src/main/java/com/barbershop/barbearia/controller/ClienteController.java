f690a436357f4273cdc3d1699d5d6bc629c5534e
hailton costa <jhailtoncosta@gmail.com>
2022-07-26 23:07:03 -0300

    quarto commit

--- src/main/java/com/barbershop/barbearia/controller/ClienteController.java
+++ src/main/java/com/barbershop/barbearia/controller/ClienteController.java
@@ -1,8 +1,16 @@
 package com.barbershop.barbearia.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,14 +26,43 @@ public class ClienteController {
 	@GetMapping("/cadcliente")
 	public ModelAndView iniciarCliente() {
 		ModelAndView modelAndView = new ModelAndView("cadcliente");
+		modelAndView.addObject("objcliente", new Cliente());
 		return modelAndView;
 	}
 	
 	@PostMapping("/salvarcliente")
 	public ModelAndView salvarClientes(Cliente cliente) {
 		clienteRepository.save(cliente);
-		ModelAndView modelAndView = new ModelAndView("cadcliente");
+		return iniciarCliente();
+	}
+	
+	@GetMapping("/listarcliente")
+	public ModelAndView listarcliente() {
+		ModelAndView modelAndView = new ModelAndView("listarcliente");
+		modelAndView.addObject("listarcliente", clienteRepository.findAll(PageRequest.of(0, 6, Sort.by("nome"))));
 		return modelAndView;
 	}
 	
+	@GetMapping("/editarcliente/{id}")
+	public ModelAndView editarcliente(@PathVariable("id") Long id) {
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		ModelAndView modelAndView = new ModelAndView("cadcliente");
+		modelAndView.addObject("objcliente", cliente.get());
+		return modelAndView;
+	}
+	
+	@GetMapping("/excluircliente/{id}")
+	public ModelAndView excluircliente(@PathVariable("id") Long id) {
+		clienteRepository.deleteById(id);
+		return new ModelAndView("redirect:/listarcliente");
+	}
+	
+	@GetMapping("clientepag")
+	public ModelAndView carregarClientePorPaginacao(@PageableDefault(size = 6) Pageable pageable, ModelAndView modelAndView) {
+		Page<Cliente> pageCliente = clienteRepository.findAll(pageable);
+		modelAndView.addObject("listarcliente", pageCliente);
+		modelAndView.addObject("objcliente", new Cliente());
+		modelAndView.setViewName("listarcliente");
+		return modelAndView;
+	}
 }
--- src/main/java/com/barbershop/barbearia/repository/ClienteRepository.java
+++ src/main/java/com/barbershop/barbearia/repository/ClienteRepository.java
@@ -1,6 +1,9 @@
 package com.barbershop.barbearia.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.barbershop.barbearia.model.Cliente;
@@ -8,4 +11,6 @@ import com.barbershop.barbearia.model.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
 	
+	@Query("select c from Cliente c where c.nome like %?1%")
+	List<Cliente> findClienteByName(String nome);
 }
